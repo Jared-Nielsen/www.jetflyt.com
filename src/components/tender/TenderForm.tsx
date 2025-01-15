b6bb91e7c8e6abb858a1fffaf8d106e616dd0fe2
Jared Nielsen <jared@fuzionagency.com>
2025-01-14 20:11:13 -0600

    updated aircraft listings

--- src/components/tender/TenderForm.tsx
+++ src/components/tender/TenderForm.tsx
@@ -132,7 +132,7 @@ export function TenderForm({ initialData, onSubmit, onCancel }: TenderFormProps)
         <option value="">Select aircraft</option>
         {aircraft?.map(a => (
           <option key={a.id} value={a.id}>
-            {a.tail_number} - {a.manufacturer} {a.model}
+            {a.tail_number} - {a.type?.name || 'Unknown Type'}
           </option>
         ))}
       </FormSelect>
